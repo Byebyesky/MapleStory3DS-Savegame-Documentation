912edc6c89debcb7c3ec542ae1d06406bf87433e
byebyesky <Byebyesky@users.noreply.github.com>
2019-12-09 02:00:29 +0100

    Some printing and editing functionality

--- mapviewer.py
+++ mapviewer.py
@@ -17,8 +17,9 @@ RED = (0,0,255)
 COLOR = [PURPLE, GREEN, BLUE, RED, WHITE]
 
 DRAGGEDINDEX = 0
-DRAGGEDARRAY = 0
+DRAGGEDPOINT = 0
 POINTCLICKED = False
+SKIPPOINTS = False
 
 class Point:
     def __init__(self, x, y):
@@ -31,6 +32,10 @@ class Point:
     def getTuple(self):
         return (self.x, self.y)
 
+    def setXY(self, x, y):
+        self.x = x
+        self.y = y
+
 class Line:
     def __init__(self, point1, point2):
         self.p1 = point1
@@ -41,6 +46,12 @@ class Line:
 
     def getPointsXY(self):
         return (self.p1.getTuple(), self.p2.getTuple())
+
+    def setPointXY(self, x, y, point):
+        if point == 0:
+            self.p1.setXY(x,y)
+        elif point == 1:
+            self.p2.setXY(x,y)   
 
 class Element:
     def __init__(self, unk, line):
@@ -54,8 +65,8 @@ class Element:
         return (self.line.getPointsXY())
 
 class Segment:
-    def __init__(self, length):
-        self.length = length
+    def __init__(self):
+        self.length = 0
         self.elements = []
         self.end = 0
 
@@ -76,6 +87,7 @@ class Segment:
         return str(self)
 
     def addElement(self, unk, x1, y1, x2, y2):
+        self.length += 1
         p1 = Point(x1, y1)
         p2 = Point(x2, y2)
         line = Line(p1, p2)
@@ -94,13 +106,28 @@ class Segment:
             drawPoint = translateToScreenSpace(self.getPoints(i)[0])
             drawPoint2 = translateToScreenSpace(self.getPoints(i)[1])
             pygame.draw.line(screen, COLOR[color], drawPoint, drawPoint2, 3)
-         
+        
+        if not SKIPPOINTS:
+            for i in range(self.length):
+                drawPoint = translateToScreenSpace(self.getPoints(i)[0])
+                drawPoint2 = translateToScreenSpace(self.getPoints(i)[1])
+                pygame.draw.circle(screen, WHITE, drawPoint, int(4*SCALAR))
+                pygame.draw.circle(screen, WHITE, drawPoint2, int(4*SCALAR))
+
+    def findPoint(self, x, y):
+        discrepancy = 5
         for i in range(self.length):
-            drawPoint = translateToScreenSpace(self.getPoints(i)[0])
-            drawPoint2 = translateToScreenSpace(self.getPoints(i)[1])
-            pygame.draw.circle(screen, WHITE, drawPoint, int(4*SCALAR))
-            pygame.draw.circle(screen, WHITE, drawPoint2, int(4*SCALAR))
-    
+            p1, p2 = self.getPoints(i)
+            if x >= p1[0]-discrepancy and x <= p1[0]+discrepancy:
+                if y >= p1[1]-discrepancy and y <= p1[1]+discrepancy:
+                    return (i,0)
+            if x >= p2[0]-discrepancy and x <= p2[0]+discrepancy:
+                if y >= p2[1]-discrepancy and y <= p2[1]+discrepancy:
+                    return (i,1)
+
+    def changeCoordinates(self, index, point, x, y):
+        self.elements[index].line.setPointXY(x,y, point)
+
     def returnBinary(self):
         binary = struct.pack('<b', self.length)
         for i in range(self.length):
@@ -130,10 +157,13 @@ def readLineFile(fileName):
         with open(fileName, 'rb+') as f:
             segments = []
             for _ in range(4):
-                currentSeg = Segment(struct.unpack('b', f.read(1))[0])
-                for i in range(currentSeg.length):
+                currentSeg = Segment()
+                length = struct.unpack('b', f.read(1))[0]
+                for i in range(length):
                     unk,x1,y1,x2,y2 = struct.unpack('<hhhhh', f.read(10))
                     currentSeg.addElement(unk,x1,y1,x2,y2)
+                if currentSeg.length != length:
+                    raise AttributeError #oof hacky
                 f.read(1)
                 segments.append(currentSeg)
             print(segments)
@@ -165,23 +195,31 @@ else:
     fileName = sys.argv[1]
 
     segments = readLineFile(fileName)
-
+    segments[0].findPoint(0,0)
     pygame.init()
 
     FPS = 70
     DISPLAY = pygame.display.Info()
-    WIDTH = DISPLAY.current_w - 50
-    HEIGHT = DISPLAY.current_h - 50
+    WIDTH = DISPLAY.current_w - 100
+    HEIGHT = DISPLAY.current_h - 100
 
     if WIDTH < 1920 or HEIGHT < 1080:
-        WIDTH = 1920-50
-        HEIGHT = 1080-50
+        WIDTH = 1920-100
+        HEIGHT = 1080-100
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Maple Story 3DS Map-Viewer: " + fileName)
     clock = pygame.time.Clock()
 
+    font = pygame.font.SysFont(None, 36)
+    
+
     running = True
 
+    tp1 = (0,0)
+    tp2 = (0,0)
+    clicked = False
+    EDITMODE = True
+    ACTIVESEGMENT = 0
     while running:
         try:
             clock.tick(FPS)
@@ -197,41 +235,71 @@ else:
                     if event.key == pygame.K_s:
                         saveLineFile("Test.line")
                         print("File saved as: Test.line")
+                    if event.key == pygame.K_e:
+                        EDITMODE = not EDITMODE
+                        print(EDITMODE)
                     if event.key == pygame.K_c:
                         XOFFSET = 0
                         YOFFSET = 0
+                    if event.key == pygame.K_p:
+                        SKIPPOINTS = not SKIPPOINTS
+                    if event.key == pygame.K_h:
+                        SHOWHELP = not SHOWHELP
+                    if event.key == pygame.K_1:
+                        ACTIVESEGMENT = 0
+                        print(ACTIVESEGMENT)
+                    if event.key == pygame.K_2:
+                        ACTIVESEGMENT = 1
+                        print(ACTIVESEGMENT)
+                    if event.key == pygame.K_3:
+                        ACTIVESEGMENT = 2
+                        print(ACTIVESEGMENT)
+                    if event.key == pygame.K_4:
+                        ACTIVESEGMENT = 3
+                        print(ACTIVESEGMENT)
+                    
+
                 
                 if event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
                         POINTCLICKED = False
 
                 if event.type == pygame.MOUSEBUTTONDOWN:#left to move point, right to move camera, scroll to zoom
-                    """if event.button == 1:
-                        pygame.mouse.get_rel()
-                        try:
-                            mousex, mousey = translateToWorldSpace(event.pos)
-
-                            print("Coords: ", mousex, mousey)
+                    if event.button == 1:
+                        if EDITMODE == True:
+                            pygame.mouse.get_rel()
                             try:
-                                for i in range(len(point1)):
-                                    if mousex >= point1[i][0]-10 and mousex <= point1[i][0]+10:
-                                        if mousey >= point1[i][1]-10 and mousey <= point1[i][1]+10:
-                                            DRAGGEDINDEX = i
-                                            DRAGGEDARRAY = 0
-                                            POINTCLICKED = True
-                                            break
-                                if POINTCLICKED != True:
-                                    for i in range(len(point2)):
-                                        if mousex >= point2[i][0]-10 and mousex <= point2[i][0]+10:
-                                            if mousey >= point2[i][1]-10 and mousey <= point2[i][1]+10:
-                                                DRAGGEDINDEX = i
-                                                DRAGGEDARRAY = 1
-                                                POINTCLICKED = True
-                                                break
+                                mousex, mousey = translateToWorldSpace(event.pos)
+
+                                print("Coords: ", mousex, mousey)
+                                try:
+                                    ret = segments[ACTIVESEGMENT].findPoint(mousex, mousey)
+                                    if ret is not None:
+                                        print(ret)
+                                        DRAGGEDINDEX = ret[0]
+                                        DRAGGEDPOINT = ret[1]
+                                        POINTCLICKED = True
+                                except ValueError:
+                                    pass
+                            except AttributeError:
+                                pass
+                        else:
+                            pygame.mouse.get_pos()
+                            try:
+                                mousex, mousey = translateToWorldSpace(event.pos)
+                                print("Coords: ", mousex, mousey)
+
+                                if clicked == False:
+                                    tp1 = (mousex, mousey)
+                                    clicked = True
+                                elif clicked == True:
+                                    tp2 = (mousex, mousey)
+                                    segments[0].addElement(0, tp1[0], tp1[1], tp2[0], tp2[1])
+                                    clicked = False
                             except ValueError:
                                 pass
-                        except AttributeError:
-                            pass"""
+                    if event.button == 2:
+                        clicked = False
                     if event.button == 3:
                         pygame.mouse.get_rel()
                     if event.button == 4:
@@ -252,21 +320,26 @@ else:
                     if event.type == pygame.MOUSEMOTION:
                         if POINTCLICKED == True:
                             posX, posY = translateToWorldSpace(event.pos)
-                            if DRAGGEDARRAY == 0:
-                                if posX >= 0 and posY >= 0 and posX <= MAXOFFSET and posY <= MAXOFFSET:
-                                    point1[DRAGGEDINDEX][0] = posX
-                                    point1[DRAGGEDINDEX][1] = posY
-                            if DRAGGEDARRAY == 1:
-                                if posX >= 0 and posY >= 0 and posX <= MAXOFFSET and posY <= MAXOFFSET:
-                                    point2[DRAGGEDINDEX][0] = posX
-                                    point2[DRAGGEDINDEX][1] = posY
-
+                            if posX >= 0 and posY >= 0 and posX <= MAXOFFSET and posY <= MAXOFFSET:
+                                segments[ACTIVESEGMENT].changeCoordinates(DRAGGEDINDEX, DRAGGEDPOINT,posX, posY)
+                
                 if pygame.mouse.get_pressed()[2]: #check for right mouse button
                     if event.type == pygame.MOUSEMOTION:
                         movementX, movementY = event.rel
                         XOFFSET += movementX
                         YOFFSET += movementY
                 
+
+            if EDITMODE == False:
+                currLoc = pygame.mouse.get_pos()
+                pygame.draw.circle(screen, WHITE, currLoc, int(4*SCALAR))
+            if clicked == True:
+                pygame.draw.circle(screen, WHITE, translateToScreenSpace(tp1), int(4*SCALAR))
+            
+            activeSegText = font.render("Activesegment: {}".format(ACTIVESEGMENT), True, WHITE)
+            screen.blit(activeSegText, (0,HEIGHT - activeSegText.get_height()))
+            editModeText = font.render("Editmode: {}".format(EDITMODE), True, WHITE)
+            screen.blit(editModeText, (0 + activeSegText.get_width() + 20,HEIGHT - editModeText.get_height()))
             pygame.display.flip()
         except KeyboardInterrupt:
             running = False
